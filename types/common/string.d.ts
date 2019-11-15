3298b095f1d623c9a9f781627abf022fcc011fad
Jordan Sexton <jordan@jordansexton.com>
2019-11-15 16:01:44 -0600

    initial commit

---
+++ types/common/string.d.ts
@@ -0,0 +1,24 @@
+/**
+ * A Base64-encoded string.
+ */
+export type Base64String = string;
+
+/**
+ * A "raw" binary-encoded string.
+ */
+export type BinaryString = string;
+
+/**
+ * A JSON-encoded string.
+ */
+export type JSONString = string;
+
+/**
+ * A hexadecimal-encoded string
+ */
+export type HexString = string;
+
+/**
+ * A Unicode Scalar Values-encoded string
+ */
+export type USVString = string;
